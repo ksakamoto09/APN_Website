194660d4158f65ebf42c15281d07919bdc519967
ks2890 <ksakamoto09@gmail.com>
2021-07-23 13:29:36 -0700

    fixing bios

--- helperFunctions.R
+++ helperFunctions.R
@@ -28,16 +28,16 @@ memberBio <- function(first, last, authorURL, bioImage, program, programURl,
     siteURL <- paste0(stringr::str_to_lower(city),".html")
     location <- paste0(city, ", ", country)
     div(class="col-md-4",
-        a(href="#", `data-featherlight`=paste0("#bio-",last),
+        a(href="#", `data-featherlight`=paste0("#bio-",stringr::str_remove_all(name, "[^[:alnum:]]")),
           img(src=bioImage, width="200", height="200")),
         p(tyle="text-align:left; color:#727272;", name),
         div(
             div(class = "lightbox", 
-                id=paste0("bio-", last),
+                id=paste0("bio-", stringr::str_remove_all(name, "[^[:alnum:]]")),
                 if(!is.na(authorURL)) a(href=authorURL, h3(name))
                     else h3(name),
                 p(style="font-family:Lato; font-weight:500; font-style:normal; 
-                  font-size:12px; letter-spacing:.4px; line-height:2.18em; color:#999999;",
+                  font-size:14px; letter-spacing:.4px; line-height:2.18em; color:#999999;",
                   a(href = universityURL, university),
                   br(),
                   if(!is.na(programURl)) a(href=programURl, program)
@@ -45,9 +45,8 @@ memberBio <- function(first, last, authorURL, bioImage, program, programURl,
                   br(),
                   location,
                   br(),
-                  a(href = email, "email"),
+                  a(href = email, "Email"),
                   br(),
-                  " - ",
                   a(href = siteURL, "APN City Page"),
                   br(),
                   p(bio)))))
@@ -81,38 +80,4 @@ cityEach <- function(city, df){
         pull(Email)
     
     cityBio(city, emails %>%  map(~memberEach(.x, df)))
-}
-
-    
-################################
-##### Publications Section #####
-################################
-
-# this creates a section for each publication Type
-pubType <- function(pub, pubDescription, ...){
-    if(length(list(...))>4){
-        stop("too many papers, has to be less than 4")
-    }
-
-    div(class="bandContent gallerySection",
-        div(class="gallerySectionTitle", pub),
-        div(class="galleryIntro", pubDescription),
-        div(class="galleryItems",
-            ...
-        )
-    )
-}
-
-# this creates a icon for each paper
-# with a description
-paperInfo <- function(link, paperImage, title, authors, summary){
-    div(class="galleryItem",
-        a(href=link,
-          img(class="galleryItemImage", src=paperImage)),
-        div(class="galleryItemDescription",
-            a(href=link,
-              p(title)),
-            strong(authors),
-            em(summary))
-        )
 }
